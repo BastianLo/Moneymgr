cb5b56b009376b742d964c91461da0abb8c5e4fe
BastianLo <bastian.lobe@t-online.de>
2023-05-20 13:51:57 +0200

    Initial project

---
+++ backend/src/main/java/bastianlo/moneymgr/MoneymgrApplication.java
@@ -0,0 +1,13 @@
+package bastianlo.moneymgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymgrApplication.class, args);
+	}
+
+}
